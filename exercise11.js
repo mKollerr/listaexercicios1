2cd78efd2325557e805e71d5b9c70728ae9f504f
Matheus Koller <kg.matheus@gmail.com>
2025-05-07 18:23:10 -0300

    exercise11 complete

---
+++ exercise11.js
@@ -0,0 +1,11 @@
+//Escreva um programa que solicita ao usuário 5 números e calcula a soma total utilizando um loop for.
+const prompt = require('prompt-sync')()
+
+let soma = 0
+
+for(let i = 0; i < 5; i++) {
+    let num = Number(prompt('Informe um número:'))
+    soma += num
+}
+
+console.log(`A soma dos 5 números informados é ${soma}.`);
